349b73ac915b0da00c08632c981517bc32e6e3b4
Mike Hume <1088457+mhumeSF@users.noreply.github.com>
2026-04-08 22:36:55 -0400

    update secrets

--- vms/k3s.nix
+++ vms/k3s.nix
@@ -70,6 +70,10 @@ in {
     "/movies".neededForBoot = true;
   };
 
+  boot.initrd.secrets = {
+    "/persist/ssh/ssh_host_ed25519_key" = "/var/lib/microvms/k3s/persist/ssh/ssh_host_ed25519_key";
+  };
+
   services.openssh.hostKeys = [
     { path = "/persist/ssh/ssh_host_ed25519_key"; type = "ed25519"; }
     { path = "/persist/ssh/ssh_host_rsa_key"; type = "rsa"; bits = 4096; }
