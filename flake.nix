ed1a78aefe4bb70aaaa8ca0fb8f05edd4d7feb19
Mike Hume <1088457+mhumeSF@users.noreply.github.com>
2026-06-14 08:42:28 -0400

    Upgrade nixpkgs from 25.11 to 26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     microvm.url = "github:astro/microvm.nix";
